360390e90f403cc7b27948e1568ae5586b9b21ee
Kyeong Lee <kaythechemist@gmail.com>
2017-10-22 16:42:23 +0900

    first function test note was added and unittest was used

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,34 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # confirming website
+        self.browser.get('http://localhost:8000')
+
+        # 'To-Do' has written on title and header.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
+        # when you want to add "buy feathers" on the list,
+
+        # you can write it on the textbox.
+
+        # when you push enter key,
+        # page is updated and "1: buy feathers" is written on the list.
+
+        # And there is additional textbox to insert additional item.
