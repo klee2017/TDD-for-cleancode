c637f6cea2ecda8424395e17092afdf1ac9caa23
Kyeong Lee <kaythechemist@gmail.com>
2017-12-08 14:19:51 +0900

    self.header_text -> header_text

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -13,24 +13,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # confirming website
+        # when you heard about new application for working list,
+        # you would confirm that website.
         self.browser.get('http://localhost:8000')
 
         # 'To-Do' has written on title and header.
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', self.header_text)
+        self.assertIn('To-Do', header_text)
 
         # work is added
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'working item insert'
+            'inserting working item'
         )
 
         # when you want to add "buy feathers" on the list,
         # you can write it on the textbox.
-        inputbox.send_keys('buy feather')
+        inputbox.send_keys('buying feather')
 
         # when you push enter key,
         # page is updated and "1: buy feathers" is written on the list.
@@ -39,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: buy feather' for row in rows),
+            any(row.text == '1: buying feather' for row in rows),
         )
 
         # And there is additional textbox to insert additional item.
